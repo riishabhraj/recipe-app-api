490438d796f1140f66b36558b89df350d2a053ee
riishabhraj <rishabhraj5421@gmail.com>
2025-04-06 22:25:45 +0530

    configuring database

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """
+    Add two numbers
+    """
+    return x + y
+
+def subtract(x, y):
+    """
+    Subtract two numbers
+    """
+    return x - y
