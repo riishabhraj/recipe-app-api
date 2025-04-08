55ced1ad059b206c6c81c8e4ce8d068ae05a8cbf
riishabhraj <rishabhraj5421@gmail.com>
2025-04-09 00:36:07 +0530

    fixed issues

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,11 +2,13 @@
 Calculator functions
 """
 
+
 def add(x, y):
     """
     Add two numbers
     """
     return x + y
+
 
 def subtract(x, y):
     """
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -8,6 +8,7 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """Django command to wait for the database to be available."""
 
