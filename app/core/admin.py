01275d443b8b8bc4f6bdc14eb307d0d100ac60a3
riishabhraj <rishabhraj5421@gmail.com>
2025-04-14 16:51:14 +0530

    added test and logics for creating a user

--- app/core/admin.py
+++ app/core/admin.py
@@ -18,6 +18,12 @@ class UserAdmin(BaseUserAdmin):
         ),
         (_('Important dates'), {'fields': ('last_login',)}),
     )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_active', 'is_staff', 'is_superuser', 'name'),
+        }),
+    )
     readonly_fields = ['last_login']
 
 
